0da76d524040a1d1510da9ead88cc37607af5837
juan190963 <136944195+juan190963@users.noreply.github.com>
2024-01-13 15:05:35 -0500

    Add files via upload

---
+++ js/tranciciones.js
@@ -0,0 +1,112 @@
+window.sr = ScrollReveal();
+
+  sr.reveal('.nav_logo',{
+    duration:800,
+    orgin: 'bottom',
+    distance:'-100px'
+  });
+
+  sr.reveal('.nav_link',{
+    duration:800,
+    origin: 'bottom',
+    distance:'100px'
+  });
+
+  sr.reveal('.banner_title',{
+    duration:800,
+    origin: 'bottom',
+    distance:'100px'
+  });
+
+  sr.reveal('.banner_img',{
+    duration:800,
+    origin: 'bottom',
+    distance:'-100px'
+  });
+
+  sr.reveal('.banner_arrow',{
+    duration:3000,
+    origin: 'bottom',
+    distance:'400px'
+  });
+
+  sr.reveal('.subtitulo',{
+    duration:800,
+    origin: 'bottom',
+    distance:'100px'
+  });
+
+  sr.reveal('.about_parrafo_1',{
+    duration:800,
+    origin: 'bottom',
+    distance:'-100px'
+  });
+
+  sr.reveal('.about_icono',{
+    duration:2000,
+    origin: 'left',
+    distance:'400px'
+  });
+
+  sr.reveal('.about_icono1',{
+    duration:2000,
+    origin: 'rigth',
+    distance:'400px'
+  });
+
+  sr.reveal('.about_title',{
+    duration:2000,
+    origin: 'rigth',
+    distance:'400px'
+  });
+
+  sr.reveal('.about_parrafo',{
+    duration:2000,
+    origin: 'left',
+    distance:'400px'
+  });
+
+  sr.reveal('.subtitulo1',{
+    duration:2000,
+    origin: 'bottom',
+    distance:'100px'
+  });
+
+  sr.reveal('.zonas_review',{
+    duration:3000,
+    origin: 'bottom',
+    distance:'-100px'
+  });
+
+  sr.reveal('.zonas_picture',{
+    duration:3000,
+    origin: 'right',
+    distance:'100px'
+  });
+
+  sr.reveal('.zonas_arrow',{
+    duration:200,
+    origin: 'left',
+    distance:'100px'
+  });
+
+  sr.reveal('.zonas_arrow1',{
+    duration:800,
+    origin: 'rigth',
+    distance:'100px'
+  });
+
+  sr.reveal('.footer',{
+    duration:4000,
+    orgin: 'bottom',
+    distance:'100px'
+  });
+
+  
+
+  
+
+
+  
+
+  
